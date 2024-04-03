69accf3d0964dc581aafbd9b5b9bfd73cfdd368f
Kulagin Anatoliy <talkafk@gmail.com>
2024-04-03 23:19:59 +0400

    renaming

--- addons/WebBusPlugin/Demo/Demo.gd
+++ addons/WebBusPlugin/Demo/Demo.gd
@@ -1,46 +1,46 @@
 extends Control
 
 func _ready():
-	WebSDK.ad_closed.connect(ad_closed)
-	WebSDK.ad_error.connect(ad_error)
-	WebSDK.ad_started.connect(ad_started)
-	WebSDK.reward_added.connect(reward_added)
+	WebBus.ad_closed.connect(ad_closed)
+	WebBus.ad_error.connect(ad_error)
+	WebBus.ad_started.connect(ad_started)
+	WebBus.reward_added.connect(reward_added)
 
 
 func _on_show_ad_pressed():
-	WebSDK.show_ad()
+	WebBus.show_ad()
 
 
 func _on_show_reward_ad_pressed():
-	WebSDK.show_rewarded_ad()
+	WebBus.show_rewarded_ad()
 
 
 func _on_show_banner_pressed():
-	WebSDK.show_banner()
+	WebBus.show_banner()
 
 
 func _on_hide_banner_pressed():
-	WebSDK.hide_banner()
+	WebBus.hide_banner()
 
 
 func _on_get_language_pressed():
-	var lang = WebSDK.get_language()
+	var lang = WebBus.get_language()
 	print("language:", lang)
 	
 
 
 func _on_get_type_device_pressed():
-	var type = WebSDK.get_type_device()
+	var type = WebBus.get_type_device()
 	print("device type:", type)
 
 
 func _on_set_yandex_leaderboard_pressed():
 	var name_leaderboard :String = $VBoxContainer/HBoxContainer2/leaderboard.text
 	var score: int = int($VBoxContainer/HBoxContainer2/score.text)
-	WebSDK.set_yandex_leaderboard(name_leaderboard, score)
+	WebBus.set_yandex_leaderboard(name_leaderboard, score)
 	
 func _on_yandex_ready_pressed():
-	WebSDK.yandex_ready()
+	WebBus.yandex_ready()
 
 func ad_closed():
 	print("ad_closed")
@@ -58,9 +58,9 @@ func reward_added():
 var leaderboard_info
 
 func _on_get_yandex_leaderboard_info_pressed():
-	WebSDK.leaderboard_info_recieved.connect(getting_leaderboard_info)
+	WebBus.leaderboard_info_recieved.connect(getting_leaderboard_info)
 	var name_leaderboard :String = $VBoxContainer/HBoxContainer/leaderboard.text
-	WebSDK.get_leaderboard_info(name_leaderboard)
+	WebBus.get_leaderboard_info(name_leaderboard)
 	
 	
 func getting_leaderboard_info(info):
@@ -70,20 +70,20 @@ func getting_leaderboard_info(info):
 
 
 func _on_happytime_pressed():
-	WebSDK.crazy_happytime()
+	WebBus.crazy_happytime()
 
 
 func _on_start_gameplay_pressed():
-	WebSDK.crazy_start_gameplay()
+	WebBus.crazy_start_gameplay()
 
 
 func _on_stop_gameplay_pressed():
-	WebSDK.crazy_stop_gameplay()
+	WebBus.crazy_stop_gameplay()
 
 
 func _on_start_loading_pressed():
-	WebSDK.crazy_start_loading()
+	WebBus.crazy_start_loading()
 
 
 func _on_stop_loading_pressed():
-	WebSDK.crazy_stop_loading()
+	WebBus.crazy_stop_loading()
--- addons/WebBusPlugin/WedBusPlugin.gd
+++ addons/WebBusPlugin/WedBusPlugin.gd
@@ -1,14 +1,14 @@
 @tool
 extends EditorPlugin
 
-const  AUTOLOAD_NAME_AD = "WebSDK"
+const  AUTOLOAD_NAME_AD = "WebBus"
 
 var export_plugin: HTMLExportPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_autoload_singleton(AUTOLOAD_NAME_AD, "res://addons/WebSDKPlugin/WebSDK.gd")
-	export_plugin = load("res://addons/WebSDKPlugin/export_plugin.gd").new()
+	add_autoload_singleton(AUTOLOAD_NAME_AD, "res://addons/WebSDKPlugin/WebBus.gd")
+	export_plugin = load("res://addons/WebBusPlugin/export_plugin.gd").new()
 	add_export_plugin(export_plugin)
 
 
