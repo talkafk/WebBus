5eb48de31f36475f8c8b9447d851aea19a963e4f
Kulagin Anatoliy <talkafk@gmail.com>
2024-04-05 22:25:38 +0400

    fix

--- addons/WebBusPlugin/WedBusPlugin.gd
+++ addons/WebBusPlugin/WedBusPlugin.gd
@@ -7,7 +7,7 @@ var export_plugin: HTMLExportPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_autoload_singleton(AUTOLOAD_NAME_AD, "res://addons/WebSDKPlugin/WebBus.gd")
+	add_autoload_singleton(AUTOLOAD_NAME_AD, "res://addons/WebBusPlugin/WebBus.gd")
 	export_plugin = load("res://addons/WebBusPlugin/export_plugin.gd").new()
 	add_export_plugin(export_plugin)
 
