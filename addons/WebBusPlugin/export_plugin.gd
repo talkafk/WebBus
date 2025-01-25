007683b41b08a59d06a351b9443e4cfc0d696472
Anatoly Kulagin <talkafk@gmail.com>
2025-01-25 20:48:41 +0100

    Fix crash non-web builds

--- addons/WebBusPlugin/export_plugin.gd
+++ addons/WebBusPlugin/export_plugin.gd
@@ -10,6 +10,7 @@ var CRAZY_BANNER_STYLE = "width: 720px; height: 90px; position: fixed; bottom:0;
 
 var plugin_path: String = get_script().resource_path.get_base_dir()
 var export_path := ""
+var _features: Array
 
 var crazy_banner_w := "728px"
 var crazy_banner_h := "90px"
@@ -22,29 +23,31 @@ func _get_name() -> String:
 
 func _export_begin(features: PackedStringArray , is_debug: bool, path: String, flags: int) -> void:
 	export_path = path
+	_features = features
 
 
 func _export_end() -> void:
-	var file := FileAccess.open(export_path, FileAccess.READ)
-	var html := file.get_as_text()
-	file.close()
-	var pos = html.find('</head>')
-	
-	html = html.insert(pos, 
-			'<script src="' + JS_FILE + '"></script>\n')
-	DirAccess.open(".")\
-			.copy(plugin_path + '/' + JS_FILE,\
-			export_path.get_base_dir() + '/' + JS_FILE)
-	CRAZY_BANNER_STYLE = "width: " + str(crazy_banner_w) + "; height: " + str(crazy_banner_h) + "; position: fixed; " + crazy_banner_pos + ":0; z-index:9999"
-	html = html.insert(pos,
-			'<div id="responsive-banner-container" hidden="hidden" style="'
-			 + CRAZY_BANNER_STYLE +'"></div>'
-			)
-	file = FileAccess.open(export_path, FileAccess.WRITE)
-	file.store_string(html)
-	file.close()
-	if is_archive:
-		zip_export(ARCHIVE_NAME)
+	if "web" in _features:
+		var file := FileAccess.open(export_path, FileAccess.READ)
+		var html := file.get_as_text()
+		file.close()
+		var pos = html.find('</head>')
+		
+		html = html.insert(pos, 
+				'<script src="' + JS_FILE + '"></script>\n')
+		DirAccess.open(".")\
+				.copy(plugin_path + '/' + JS_FILE,\
+				export_path.get_base_dir() + '/' + JS_FILE)
+		CRAZY_BANNER_STYLE = "width: " + str(crazy_banner_w) + "; height: " + str(crazy_banner_h) + "; position: fixed; " + crazy_banner_pos + ":0; z-index:9999"
+		html = html.insert(pos,
+				'<div id="responsive-banner-container" hidden="hidden" style="'
+				+ CRAZY_BANNER_STYLE +'"></div>'
+				)
+		file = FileAccess.open(export_path, FileAccess.WRITE)
+		file.store_string(html)
+		file.close()
+		if is_archive:
+			zip_export(ARCHIVE_NAME)
 	
 	
 	
